500fd9b69d2086744d2f53478bc7b95152f35013
Debajit Majumder <debajit.business@gmail.com>
2016-11-03 17:10:30 +0530

    tried out express-session to determine page-views

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 var express = require('express'),
 	morgan  = require('morgan'), // for logging, specially on dev environment
 	port 	= process.env.PORT || 3000,
+	session = require('express-session'),
+	parseurl= require('parseurl'),
 	app 	= express(); // creating the app
 
 /* Middlewares - app.use - they execute sequentially */
 app.use(morgan('dev'));
 
+app.use(session({
+	secret : 'mysecret',
+	saveUninitialized : true,
+	resave : true
+}));
+
+//Get page views using express session - using custom middleware
+app.use(function( req, res, next ) {
+	var pageViews = req.session.views,
+	    pathName  = parseurl( req ).pathname;
+
+	if( !pageViews ) {
+		pageViews = req.session.views = {};	
+	}
+	pageViews[ pathName ] = (pageViews[pathName] || 0) + 1;
+	next();
+});
+
 app.use('/', function( req, res, next) {
-	res.send('Welcome to Express world!');
+	res.send('Welcome to Express world! You visited this page ' + req.session.views['/'] + ' times');
 });
 
 app.listen( port, ()=> {
